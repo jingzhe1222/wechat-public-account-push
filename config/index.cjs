9ed7f87e27dd372907b2c74d02e8da14093e6f06
jingzhe1222 <155628114+jingzhe1222@users.noreply.github.com>
2024-01-04 20:32:03 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -51,7 +51,7 @@ const USER_CONFIG = {
 
 
   // 【推送完成提醒】模板id, 用来看自己有没有发送成功的那个模板
-  CALLBACK_TEMPLATE_ID: 'M0CoQJaDyuywAcH5rHA6u1E5Hpv_XeSxtSy1j_pJVdo',
+  CALLBACK_TEMPLATE_ID: '	4mYqBw8e_qJv_VTPsAf81Cn0JMjc8uk_Gshg4puOqbc',
 
   CALLBACK_USERS: [
     {
