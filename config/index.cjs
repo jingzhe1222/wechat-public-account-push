1db1e36535e889799ac21ea50b7cb0dfaa1d12c6
jingzhe1222 <155628114+jingzhe1222@users.noreply.github.com>
2024-01-04 19:56:44 +0800

    1127Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -23,7 +23,7 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'o-6kt6-TmVQaZ7nXEZ5LnCi5L3wc',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: '3TPP03CnbwG29xMuABJVSG9rGkZvZ0XCvvf6yF10STI',
+      useTemplateId: 'M0CoQJaDyuywAcH5rHA6u1E5Hpv_XeSxtSy1j_pJVdo',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '11-27',
       festivals: [
