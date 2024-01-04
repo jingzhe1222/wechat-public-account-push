6eebaf45e93e2eae20a873ce5df5f88bab674ea8
jingzhe1222 <155628114+jingzhe1222@users.noreply.github.com>
2024-01-04 21:14:19 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -14,7 +14,7 @@ const USER_CONFIG = {
   APP_SECRET: '4446086088ac9762932d7302d0bddebc',
 
   PROVINCE: '安徽',
-  CITY: '合肥',
+  CITY: '宣城',
 
   USERS: [
     {
@@ -23,7 +23,7 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'o-6kt6-TmVQaZ7nXEZ5LnCi5L3wc',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: '	4mYqBw8e_qJv_VTPsAf81Cn0JMjc8uk_Gshg4puOqbc',
+      useTemplateId: '4mYqBw8e_qJv_VTPsAf81Cn0JMjc8uk_Gshg4puOqbc',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '11-27',
       festivals: [
